ed15caa3a775f00a7b873ef14d88d999c970595a
Robert Buchkowski <robert.buchkowski@gmail.com>
2023-04-28 11:31:49 -0300

    Correcting authors and copyright for Cheddar function.

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -2,10 +2,48 @@
 #'
 #' @param W A matrix of trophic species. Rows eat columns.
 #' @return A vector of trophic level assignments. The base of the food chain is 0.
+#' @details
+#' This function is a subset of code provided in the package Cheddar written by Lawrence Hudson, Dan Reuman and Rob Emerson. It is licensed under a BSD_2_clause, the text of which is provided as a comment in the function code. The original package can be found on CRAN or https://github.com/quicklizard99/cheddar/
 #' @export
 #' @examples
 #' TLcheddar(intro_comm$imat)
 TLcheddar <- function(W){
+
+  # Original license for this code:
+  # YEAR:2014-2016
+  # COPYRIGHT HOLDER:  Lawrence Hudson, Dan Reuman and Rob Emerson
+  #
+  #   and specify as
+  #
+  # License: BSD_2_clause + file LICENSE
+  #
+  # Copyright (c) 2014-2016, Lawrence Hudson, Dan Reuman and Rob Emerson
+  #
+  #   Redistribution and use in source and binary forms, with or without
+  # modification, are permitted provided that the following conditions are
+  # met:
+  #
+  #   Redistributions of source code must retain the above copyright
+  # notice, this list of conditions and the following disclaimer.
+  #
+  # Redistributions in binary form must reproduce the above copyright
+  # notice, this list of conditions and the following disclaimer in
+  # the documentation and/or other materials provided with the
+  # distribution.
+  #
+  # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  # A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  # HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  # SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  # LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  # DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
   # Make each row sum to one.
   rs <- rowSums(W)
   W <- W / matrix(rs, ncol=ncol(W), nrow=nrow(W))
